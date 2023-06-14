eed8f007ca7755fdcc4bdae5dd3876850df9b26f
Clukva <yura4-1-1@mail.ru>
2023-06-14 14:47:09 +0300

    feat: add delete.js solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+
+const deletePath = "src/fs/files/fileToRemove.txt";
+
 const remove = async () => {
-    // Write your code here 
+  if (!fs.existsSync(deletePath)) {
+    throw new Error("FS operation failed");
+  }
+  return new Promise((res, rej) => {
+    fs.unlink(deletePath, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        console.log("File removed");
+        res();
+      }
+    });
+  });
 };
 
 await remove();
+
