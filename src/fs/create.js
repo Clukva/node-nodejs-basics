15e9a0e80f707a3166f3ce2c1c336355d1db1293
Clukva <yura4-1-1@mail.ru>
2023-06-14 13:38:34 +0300

    feat: add create.js and copy.js solutions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = "src/fs/files/fresh.txt";
+  const fileContent = "I am fresh and young";
+
+  if (fs.existsSync(filePath)) {
+    throw new Error("FS operation failed");
+  }
+  return new Promise((res, rej) => {
+    fs.writeFile(filePath, fileContent, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        console.log("File created");
+        res();
+      }
+    });
+  });
 };
 
 await create();
+
