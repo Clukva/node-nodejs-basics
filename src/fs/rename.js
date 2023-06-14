6f2aab69c7d99bd2642ef8e9bc795c6feffdfa0f
Clukva <yura4-1-1@mail.ru>
2023-06-14 14:32:00 +0300

    feat: add rename.js solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
+const renameFrom = "src/fs/files/wrongFilename.txt";
+const renameTo = "src/fs/files/properFilename.md";
+
 const rename = async () => {
-    // Write your code here 
+  if (fs.existsSync(renameTo) || !fs.existsSync(renameFrom)) {
+    throw new Error("FS operation failed");
+  }
+  return new Promise((res, rej) => {
+    fs.rename(renameFrom, renameTo, (err) => {
+      if (err) {
+        rej(err);
+      } else {
+        console.log("File renamed");
+        res();
+      }
+    });
+  });
 };
 
 await rename();
+
