01cc9e4f8eb4fa03e52f587c97266e194a4e6bc3
Clukva <yura4-1-1@mail.ru>
2023-06-15 21:12:32 +0300

    feat: add args.js solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argsArray = [];
+
+  for (let i = 0; i < process.argv.length; i++) {
+    if (process.argv[i].startsWith("--")) {
+      argsArray.push(process.argv[i].slice(2) + " is " + process.argv[i + 1]);
+    }
+  }
+
+  console.log(argsArray.join(", "));
 };
 
 parseArgs();
+
