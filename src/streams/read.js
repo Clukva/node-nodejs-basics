2462eb56c296fffc976c88767d2e0b1e3928bd25
Clukva <yura4-1-1@mail.ru>
2023-06-17 13:06:55 +0300

    feat: add transform.js solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { stdout } from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    let dataContent = "";
+    const myReadStream = fs.createReadStream(
+      __dirname + "/files/fileToRead.txt",
+      "utf-8"
+    );
+    myReadStream.on("data", (chunk) => {
+      dataContent += chunk;
+    });
+    myReadStream.on("end", () => {
+      stdout.write(dataContent);
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await read();
+
