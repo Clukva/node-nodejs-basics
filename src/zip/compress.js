f0676d91e026f0bc79181cd4212342409712acb2
Clukva <yura4-1-1@mail.ru>
2023-06-17 23:54:56 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import zlib from "zlib";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesDir = __dirname + "/files";
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const inputStream = fs.createReadStream(filesDir + "/fileToCompress.txt");
+  const outputStream = fs.createWriteStream(filesDir + "/archive.gz");
+
+  inputStream.pipe(gzip).pipe(outputStream);
 };
 
 await compress();
+
